16a9537eb6a1ecb2503dc30a6283f4ada02bb2e9
Joshua <mwalejoshua89@gmail.com>
2022-10-27 10:08:13 +0300

    wrote tests for session model and sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :announcements, only: [:index ] #:create]
   resources :comments, only: [:index, :create]
-  resources :sessions
-  resources :cohorts
+  resources :sessions, only: [:index, :create, :show, :update, :destroy]
+  resources :cohorts, only: [:index, :create, :show, :update, :destroy]
   resources :students, only: [:index, :create]
   resources :technical_mentors, only: [:index, :create]
  
