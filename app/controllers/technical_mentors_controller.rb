cb29a2829275aa3f2eba8a7c6b558f331fa9c350
Joshua <mwalejoshua89@gmail.com>
2022-10-28 21:00:02 +0300

    wrote code for jwt authentication of technical mentor

--- app/controllers/technical_mentors_controller.rb
+++ app/controllers/technical_mentors_controller.rb
@@ -1,34 +1,73 @@
 class TechnicalMentorsController < ApplicationController
-    skip_before_action :authorized, only: [:create]
+    #before_action :authorize_student
+    #skip_before_action :authorize_student, only: [:create]
 
-    def index
-        technical_mentors=TechnicalMentor.all
-        render json: technical_mentors
-    end
+ # data encoding
+ def encode_token(email, password)
+    expiry = Time.now.to_i + 1800
+    data = {
+        "email": email,
+        "password": password,
+        "expiry": expiry
+    }
+    JWT.encode(data, "my_secr8t")
+end
 
-    def create
-        technical_mentor=@current_tm.technical_mentors.create!(technical_mentor_params)
-        render json: technical_mentor, status: :created
-
-        @technical_mentor = TechnicalMentor.create(technical_mentor_params)
-        if @technical_mentor.valid?
-            @token = encode_token(technical_mentor_id: @technical_mentor_id)
-            render json: { technical_mentor: TechnicalMentorSerializer.new(@technical_mentor), jwt: @token }, status: :created
-        else
-            render json: { error: 'Invalid email or password' }, status: :unprocessable_entity
+#data decoding
+def decode_token
+    auth_header_jwt = request.headers['Authorization']
+    if auth_header_jwt
+        token = auth_header_jwt.split(' ')[1]
+        #header is = {'Authorization: 'Bearer <token>'}
+        begin
+            JWT.decode(token, "my_secr8t", true, algorithm: 'HS256')
+        rescue JWT::DecodeError
+            nil
         end
     end
+end
+      
 
-
-    @technical_mentor = TechnicalMentor.find_by(email: technical_mentor_login_params[:email])
-    #Student#authenticate comes from BCrypt
-    if @technical_mentor && @technical_mentor.authenticate(technical_mentor_login_params[:password])
-        #encode token is from ApplicationController
-        token = encode_token({ technical_mentor_id: @technical_mentor.id})
-        render json: { technical_mentor: TechnicalMentorSerializer.new(@technical_mentor), jwt: token}, status: :accepted
-    else
-        render json: { message: 'Invalid email or password' }, status: :unprocessable_entity
+def authorized_technical_mentor
+    decoded_token = decode_token()
+    if decoded_token 
+        technical_mentor_id = decoded_token[0]['technical_mentor_id']
+        technical_mentor = TechnicalMentor.find_by(id: technical_mentor_id)
     end
+end 
+
+def technical_mentor_logged_in?
+    !!authorized_technical_mentor
+end
+
+def authorize_technical_mentor
+    render json: { message: 'Technical Mentor only.' }, status: :unauthorized unless technical_mentor_logged_in?
+end
+
+def create
+    technical_mentor = TechnicalMentor.create!(technical_mentor_params)
+    if technical_mentor.valid?
+        token = encode_token({ technical_mentor_id: technical_mentor.id})
+        render json: { technical_mentor: technical_mentor, token: token }, status: :ok
+    else
+        render json: { error: 'Invalid email or password' }, status: :unprocessable_entity
+    end
+end
+
+def login 
+    technical_mentor = TechnicalMentor.find_by(email: params[:email])
+    #TechnicalMentor#authenticate comes from BCrypt
+    if technical_mentor && technical_mentor.authenticate(params[:password])
+        token = encode_token({ technical_mentor_id: technical_mentor.id })
+        render json: { technical_mentor: technical_mentor, token: token}, status: :ok
+    else
+        render json: { error: 'Invalid email or password' }, status: :unprocessable_entity
+    end
+end
+
+
+def index
+    render json: TechnicalMentor.all
 end
 
 def logout
@@ -38,18 +77,9 @@ def logout
     }, status: 200
 end
 
+private 
 
-private
-
-    def technical_mentor_params
-        params.require(:technical_mentor).permit(:name, :email, :phone)
-    end
-
-
-    def technical_mentor_login_params 
-        # params { student: {email: 'ianokumu@gmail.com', password: 'Heyguyz254' }}
-        params.(:technical_mentor).permit(:email, :password)
-    end
+def technical_mentor_params
+    params.permit(:email, :name, :password, :password_confirmation)
 end
-
-
+end
