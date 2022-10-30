e183c4e9e7099a5f9cac15fef8e5933259cf1a61
Joshua <mwalejoshua89@gmail.com>
2022-10-30 16:11:30 +0300

    modified comments controller code

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,7 @@ class CommentsController < ApplicationController
     end
 
     def create
-        comment=@current_tm.comments.create!(comment_params)
+        comment=Comment.create!(comment_params)
         render json: comment, status: :created
     end
 
